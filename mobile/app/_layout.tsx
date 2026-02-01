d565f72d86e3a778e68fae519af4a749fda723aa
erhanikinci <erhanikinci61@gmail.com>
2026-02-02 00:18:07 +0300

    feat: add animated orb component for visual effects
    feat: implement AuthSync component for user authentication synchronization
    
    feat: create ChatItem component for displaying chat messages
    
    feat: add EmptyUI component for empty state representation
    
    feat: implement MessageBubble component for chat message display
    
    feat: create SocketConnection component for managing socket connections
    
    feat: add UserItem component for displaying user information
    
    feat: implement useAuth hook for authentication callbacks
    
    feat: create useChats hook for fetching and managing chat data
    
    feat: add useMessages hook for fetching messages in a chat
    
    feat: implement useSocialAuth hook for social authentication flows
    
    feat: create useUsers hook for fetching user data
    
    feat: add axios utility for API requests with authentication
    
    feat: implement socket management with Zustand for real-time features
    
    chore: update package.json dependencies and scripts
    
    chore: extend Tailwind CSS configuration with custom colors

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,16 +1,27 @@
 import { Stack } from "expo-router";
 import "../global.css"
 
+
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
+
+    <ClerkProvider tokenCache={tokenCache}>
+
     <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
       </Stack>
     </QueryClientProvider>
+    </ClerkProvider>
   );
 }
