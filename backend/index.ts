7065114ae5e5094f6a9f123f637328e9c442493f
erhanikinci <erhanikinci61@gmail.com>
2026-02-01 18:21:22 +0300

    feat: initialize backend with Express and MongoDB integration
    - Set up server entry point in index.ts with Bun
    - Add database connection logic in databse.ts
    - Create Express app in app.ts with health check route
    - Define models for Chat, Message, and User with Mongoose
    - Implement basic route structure for authentication, chats, messages, and users
    - Update package.json with necessary dependencies and scripts

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { connectDB } from "./src/config/databse";
+import app from "./src/app";
+
+const PORT = process.env.PORT || 3000
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('====================================');
+        console.log("Server is up and running PORT: ", PORT);
+        console.log('====================================');
+    })
+})
